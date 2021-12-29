766f2e7780e7108c0b4fb003f278acf8ea5371ec
Haris Shah <harisshah385@gmail.com>
2021-12-29 22:00:13 +0500

    adding new user

--- controllers/users.js
+++ controllers/users.js
@@ -1,16 +1,26 @@
+import { v4 as uuidv4 } from 'uuid';
+
 let users = [
 	{
 		name: 'Mike Joy',
 		email: 'mike@gmail.com',
 		age: 31,
+		id: '11',
 	},
 	{
 		name: 'Marry Joy',
 		email: 'marry@gmail.com',
 		age: 30,
+		id: '22',
 	},
 ];
 
 export const allUsers = (req, res) => {
 	res.send(users);
+};
+
+export const addNewUser = (req, res) => {
+	const user = req.body;
+	users.push({ ...user, id: uuidv4() });
+	res.send('user ' + req.body.name + ' added.');
 };
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 import express from 'express';
-import { allUsers } from '../controllers/users.js';
+import { allUsers, addNewUser } from '../controllers/users.js';
 
 const router = express.Router();
 
 // dispaly all the users
 router.get('/', allUsers);
+
+// add new user
+router.post('/', addNewUser);
 
 export default router;
